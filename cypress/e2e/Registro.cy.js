94ded49d1621c3fd87832e3cf7747ddfe40925ce
daniel diaz <gddiaz@bancolombia.com.co>
2025-08-29 20:11:16 -0500

    subiendo modulo registro

---
+++ cypress/e2e/Registro.cy.js
@@ -0,0 +1,28 @@
+describe('Test Registro web', () => {
+
+
+  beforeEach(() => {
+   cy.visit('https://ticketazo.com.ar/auth/registerUser');
+  });
+
+
+  it('passes', () => {
+   
+    cy.get('[data-cy="input-nombres"]').clear().type('daniel');
+    cy.get('[data-cy="input-apellido"]').clear().type('diaz');
+    cy.get('[data-cy="input-telefono"]').clear().type('3109292923');
+    cy.get('[data-cy="input-dni"]').clear().type('34849937');
+    cy.get('[data-cy="select-provincia"]').clear().type('Chaco{enter}');
+    cy.get('[data-cy="select-localidad"]').clear().type('Charata{enter}');
+    cy.contains('dd').type('17');
+    cy.contains('mm').type('10');
+    cy.contains('aaaa').type('1981');
+    cy.wait(1000)
+    cy.get('[data-cy="input-email"]').clear().type('daniel@hotmail.com');
+    cy.get('[data-cy="input-confirmar-email"]').clear().type('daniel@hotmail.com');
+    cy.get('[data-cy="input-password"]').clear().type('D@ni1710');
+    cy.get('[data-cy="input-repetir-password"]').clear().type('D@ni1710');
+    cy.get('[data-cy="btn-registrarse"]').click()
+    cy.wait(2000)
+  })
+})
