0921dc3703c34ba67f31fa29c45a4eeede56f084
daniel diaz <gddiaz@bancolombia.com.co>
2025-08-30 08:30:32 -0500

    cambio de contrasena

--- cypress/e2e/Registro.cy.js
+++ cypress/e2e/Registro.cy.js
@@ -1,4 +1,4 @@
-describe('Test Registro web', () => {
+describe('Test Registro Web', () => {
 
 
   beforeEach(() => {
@@ -10,7 +10,7 @@ describe('Test Registro web', () => {
    
     cy.get('[data-cy="input-nombres"]').clear().type('daniel');
     cy.get('[data-cy="input-apellido"]').clear().type('diaz');
-    cy.get('[data-cy="input-telefono"]').clear().type('3109292923');
+    cy.get('[data-cy="input-telefono"]').clear().type('3109292456');
     cy.get('[data-cy="input-dni"]').clear().type('34849937');
     cy.get('[data-cy="select-provincia"]').clear().type('Chaco{enter}');
     cy.get('[data-cy="select-localidad"]').clear().type('Charata{enter}');
@@ -18,8 +18,8 @@ describe('Test Registro web', () => {
     cy.contains('mm').type('10');
     cy.contains('aaaa').type('1981');
     cy.wait(1000)
-    cy.get('[data-cy="input-email"]').clear().type('daniel@hotmail.com');
-    cy.get('[data-cy="input-confirmar-email"]').clear().type('daniel@hotmail.com');
+    cy.get('[data-cy="input-email"]').clear().type('daniel@gmail.com');
+    cy.get('[data-cy="input-confirmar-email"]').clear().type('daniel@gmail.com');
     cy.get('[data-cy="input-password"]').clear().type('D@ni1710');
     cy.get('[data-cy="input-repetir-password"]').clear().type('D@ni1710');
     cy.get('[data-cy="btn-registrarse"]').click()
